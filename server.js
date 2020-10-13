87544fa82f11698ea10a1529349dd8e1da7f0d99
Joshua Wilensky <joshwilensky@gmail.com>
2020-10-13 13:28:16 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+const server = app.listen(PORT, () =>
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
